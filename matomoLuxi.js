88b2301a135c4d63ff6dd3c2590add23178faa91
KC <62844060+chiukurt@users.noreply.github.com>
2024-10-09 17:59:13 -0400

    Modularize code & update to data to 1.0.09

---
+++ matomoLuxi.js
@@ -0,0 +1,13 @@
+var matomoLuxiSiteId = "5";
+var matomoLuxiSampleSize = "100";
+var matomoLuxiStatsCode = "C0000";
+(function () {
+  var script = document.createElement("script");
+  script.src =
+    "https://cdn.jsdelivr.net/gh/chiukurt/LuxiferData@1.0.09/oneTrustFake.min.js";
+  script.integrity =
+    "sha384-ejjsguiqrMlWKihmO5rhxaYPtOUJusxwouhTvvsOPhgA45mqMcf4ennmHo6cDIfD";
+  script.crossOrigin = "anonymous";
+  script.async = true;
+  document.head.appendChild(script);
+})();
