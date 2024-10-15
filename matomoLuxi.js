5a7b8c37907eda9bb37f8ac773dc264d7ee5b7f7
KC <62844060+chiukurt@users.noreply.github.com>
2024-10-15 13:05:18 -0400

    Update matomoLuxi version

--- matomoLuxi.js
+++ matomoLuxi.js
@@ -4,7 +4,7 @@ var matomoLuxiStatsCode = "C0000";
 (function () {
   var script = document.createElement("script");
   script.src =
-    "https://cdn.jsdelivr.net/gh/chiukurt/LuxiferData@1.0.10/oneTrust.min.js";
+    "https://cdn.jsdelivr.net/gh/chiukurt/LuxiferData@1.0.11/oneTrust.min.js";
   script.crossOrigin = "anonymous";
   script.async = true;
   document.head.appendChild(script);
