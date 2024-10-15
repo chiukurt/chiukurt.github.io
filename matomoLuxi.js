ebae3ae61f93424f0edc09d12f46edc1f529cb7f
KC <62844060+chiukurt@users.noreply.github.com>
2024-10-15 12:40:41 -0400

    Update consent tracking * luxi script

--- matomoLuxi.js
+++ matomoLuxi.js
@@ -4,9 +4,7 @@ var matomoLuxiStatsCode = "C0000";
 (function () {
   var script = document.createElement("script");
   script.src =
-    "https://cdn.jsdelivr.net/gh/chiukurt/LuxiferData@1.0.09/oneTrustFake.min.js";
-  script.integrity =
-    "sha384-ejjsguiqrMlWKihmO5rhxaYPtOUJusxwouhTvvsOPhgA45mqMcf4ennmHo6cDIfD";
+    "https://cdn.jsdelivr.net/gh/chiukurt/LuxiferData@1.0.10/oneTrust.min.js";
   script.crossOrigin = "anonymous";
   script.async = true;
   document.head.appendChild(script);
