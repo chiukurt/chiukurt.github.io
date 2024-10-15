c336331e28f083b8f9e76851e41b12fa2015eae4
KC <62844060+chiukurt@users.noreply.github.com>
2024-10-15 13:46:49 -0400

    Add OneTrust simulation

--- matomoLuxi.js
+++ matomoLuxi.js
@@ -11,3 +11,40 @@ var _paq = window._paq = window._paq || [];
   script.async = true;
   document.head.appendChild(script);
 })();
+
+
+//Below is script to simulate a fake OneTrust implementation
+(function () { 
+  function fakeOneTrustInit() {
+    window.OnetrustActiveGroups = ["C0000"];
+    setTimeout(fakeOneTrustUpdate, 500);
+  }
+
+  function fakeOneTrustRemove() {
+    window.OnetrustActiveGroups = [];
+    setTimeout(fakeOneTrustUpdate, 500);
+  }
+
+  function fakeOneTrustUpdate() {
+    var event = new CustomEvent("OneTrustGroupsUpdated", {
+        detail: {
+            updatedGroups: OnetrustActiveGroups,
+        },
+    });
+    window.dispatchEvent(event);
+  }
+  
+  var name = "statsCode=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0 && c.substring(name.length, c.length) === "C0000") {
+        fakeOneTrustInit();
+        fakeOneTrustUpdate();
+      }
+  }
+})();
