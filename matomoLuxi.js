92c218d1b9febef786e180c70389c2acb5ddfacf
KC <62844060+chiukurt@users.noreply.github.com>
2024-11-26 18:03:47 -0500

    Update script

--- matomoLuxi.js
+++ matomoLuxi.js
@@ -4,8 +4,8 @@ var _mtm = window._mtm = window._mtm || [];
 var _paq = window._paq = window._paq || [];
 (function() {
   var script = document.createElement('script');
-  script.src = "https://cdn.jsdelivr.net/gh/chiukurt/LuxiferData@1.2.00/default.min.js";
-  script.integrity = "sha384-MI0wJ/B8rWX9Lop9XtzsM0YqL8HrewiDHsK94zbyZG860LAS9rJFANelIIwmIZNm";
+  script.src = "https://cdn.jsdelivr.net/gh/chiukurt/LuxiferData@1.2.01/default.min.js";
+  script.integrity = "sha384-esCVyMt4C70OHdbkqMCXh4r0/MpGok7xzIKu5XS5iMiOl174BR6/7TxPTtKchmC7";
   script.crossOrigin = "anonymous";
   script.async = true;
   document.head.appendChild(script);
