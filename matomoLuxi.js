bf3b971311ab77ec4a2ba5d11c86b5a5aff0c4ea
KC <62844060+chiukurt@users.noreply.github.com>
2024-11-29 15:59:15 -0500

    Update tracking script

--- matomoLuxi.js
+++ matomoLuxi.js
@@ -1,13 +1,12 @@
-var matomoLuxiSiteId = "1";
+var matomoLuxiSiteId = "6";
 var matomoLuxiSampleSize = "100";
 var _mtm = window._mtm = window._mtm || [];
 var _paq = window._paq = window._paq || [];
 (function() {
   var script = document.createElement('script');
-  script.src = "https://cdn.jsdelivr.net/gh/chiukurt/LuxiferData@1.2.01/default.min.js";
-  script.integrity = "sha384-esCVyMt4C70OHdbkqMCXh4r0/MpGok7xzIKu5XS5iMiOl174BR6/7TxPTtKchmC7";
+  script.src = "https://cdn.jsdelivr.net/gh/chiukurt/LuxiferData@1.2.02/default.min.js";
+  script.integrity = "sha384-aIRAMkKxsFX6tOA6PFhqe85yPRXNadvhxK+X5tGYVLHHrwXdvTU9ma0mio9T+3jZ";
   script.crossOrigin = "anonymous";
   script.async = true;
   document.head.appendChild(script);
 })();
-
