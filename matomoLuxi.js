5a84cc08960f9611a5bac615bef3619fef78a503
KC <62844060+chiukurt@users.noreply.github.com>
2025-01-07 16:40:08 -0500

    Add secondary MTM experiment

--- matomoLuxi.js
+++ matomoLuxi.js
@@ -10,3 +10,10 @@ var _paq = window._paq = window._paq || [];
   script.async = true;
   document.head.appendChild(script);
 })();
+
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+(function() {
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src='https://analytics.luxifer.app/js/container_1jnfkkvV.js'; s.parentNode.insertBefore(g,s);
+})();
