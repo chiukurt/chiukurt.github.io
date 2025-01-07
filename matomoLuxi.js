9c7e1a385abf15a7c06fb7c913500a55827f9d61
KC <62844060+chiukurt@users.noreply.github.com>
2025-01-07 17:43:55 -0500

    Update tag manager init

--- matomoLuxi.js
+++ matomoLuxi.js
@@ -15,5 +15,5 @@ var _mtm = window._mtm = window._mtm || [];
 _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
 (function() {
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.async=true; g.src='https://analytics.luxifer.app/js/container_1jnfkkvV.js'; s.parentNode.insertBefore(g,s);
+  g.async=true; g.src='https://analytics.luxifer.app/js/container_1jnfkkvV.js?d=' + todayParam(); s.parentNode.insertBefore(g, s);
 })();
