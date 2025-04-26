f5001c068a64b9bb4addca0062fd85c158245f38
KC <62844060+chiukurt@users.noreply.github.com>
2025-04-26 15:24:55 -0400

    Increase timeout

--- matomoLuxi.js
+++ matomoLuxi.js
@@ -133,7 +133,7 @@ _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
       removeLoadingClass();
       const end = performance.now();
       console.log(`Loading timeout -- Took ${end - start} milliseconds`);
-    }, 400); 
+    }, 500); 
 
     s.parentNode.insertBefore(g, s);
   })();
