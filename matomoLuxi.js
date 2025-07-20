840fddb1d60d0e7db63964234c7f945cd14d218e
KC <62844060+chiukurt@users.noreply.github.com>
2025-07-19 20:02:41 -0400

    Comment out test container

--- matomoLuxi.js
+++ matomoLuxi.js
@@ -23,17 +23,17 @@ var luxiAutoTimeout = setTimeout(() => {
 
 // Special MTM test container. Exclude this from client code ===========================================================
 
-function todayParam() {
-  var pad = (number) => (number < 10 ? '0' : '') + number;
-  var today = new Date();
-  return `${today.getUTCFullYear()}-${pad(today.getUTCMonth() + 1)}-${pad(today.getUTCDate())}`;
-}
-var _mtm = window._mtm = window._mtm || [];
-_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-(function() {
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.async=true; g.src='https://analytics.luxifer.app/js/container_1jnfkkvV.js?d=' + todayParam(); s.parentNode.insertBefore(g, s);
-})();
+// function todayParam() {
+//   var pad = (number) => (number < 10 ? '0' : '') + number;
+//   var today = new Date();
+//   return `${today.getUTCFullYear()}-${pad(today.getUTCMonth() + 1)}-${pad(today.getUTCDate())}`;
+// }
+// var _mtm = window._mtm = window._mtm || [];
+// _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+// (function() {
+//   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+//   g.async=true; g.src='https://analytics.luxifer.app/js/container_1jnfkkvV.js?d=' + todayParam(); s.parentNode.insertBefore(g, s);
+// })();
 
 // =====================================================================================================================
 
