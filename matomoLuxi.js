3bad55c35c9b7856d97fac84d0d1138ed6736caf
KC <62844060+chiukurt@users.noreply.github.com>
2026-06-03 21:27:29 -0400

    Add test script

--- matomoLuxi.js
+++ matomoLuxi.js
@@ -82,3 +82,9 @@
 
 // TESTING =============================================================================================================
 
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+(function() {
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src='https://analytics.luxifer.app/js/container_t9d5lGU5.js'; s.parentNode.insertBefore(g,s);
+})();
